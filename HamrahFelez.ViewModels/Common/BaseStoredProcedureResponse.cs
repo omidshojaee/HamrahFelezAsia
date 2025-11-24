bf912365f23f3861e0adfe2d9fded68f08ee5c77
Omid Shojaee <omid.shojaee@gmail.com>
2025-11-24 11:54:00 +0330

    DataAccessLayer & DataAccessManager with required models, settings and attributes

---
+++ HamrahFelez.ViewModels/Common/BaseStoredProcedureResponse.cs
@@ -0,0 +1,15 @@
+﻿namespace HamrahFelez.ViewModels.Common
+{
+    public class BaseStoredProcedureResponse
+    {
+        public BaseStoredProcedureResponse(int _result, string _message)
+        {
+            result = _result;
+            message = _message;
+            success = _result > 0;
+        }
+        public int result { get; private set; }
+        public string message { get; private set; }
+        public bool success { get; private set; }
+    }
+}
---
+++ HamrahFelez.ViewModels/Common/StoredProcedureParameter.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace HamrahFelez.ViewModels.Common
+{
+    public class StoredProcedureParameter
+    {
+        public string Name { get; private set; }
+        public object Value { get; private set; }
+        public DataTable? DataTable { get; private set; }
+        public string TvpTypeName { get; private set; }
+    }
+}
