0d02a142da62b50cc4e77b7008115a200e08fc4b
Omid Shojaee <omid.shojaee@gmail.com>
2025-12-04 16:58:36 +0330

    Added a hosted service to open database connections at startup

---
+++ HamrahFelez.Services/HostedServices/DbWarmupHostedService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Data.SqlClient;
+
+namespace HamrahFelez.Services
+{
+    public class DbWarmupHostedService : IHostedService
+    {
+        private readonly IConfiguration _configuration;
+
+        public DbWarmupHostedService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                var mainCs = _configuration.GetConnectionString("DbMain");
+                var developCs = _configuration.GetConnectionString("DbMain_Develop");
+
+                await WarmpupConnectionAsync(mainCs, "DbMain", cancellationToken);
+                await WarmpupConnectionAsync(developCs, "DbMain_Develop", cancellationToken);
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+
+        private static async Task WarmpupConnectionAsync(string connectionString, string name, CancellationToken cancellationToken)
+        {
+            if (string.IsNullOrWhiteSpace(connectionString)) return;
+
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+
+            await using var connection = new SqlConnection(connectionString);
+            await connection.OpenAsync(cancellationToken);
+
+            sw.Stop();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
--- HamrahFelez.WebAPI/Program.cs
+++ HamrahFelez.WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using HamrahFelez.Repositories;
+using HamrahFelez.Services;
 using HamrahFelez.Utilities;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHostedService<DbWarmupHostedService>();
 
 var app = builder.Build();
 
